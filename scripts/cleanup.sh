ced86f8a4cc0ab41681f11dcd8ebd9c10ec8a524
Serhii <serhiiartiukh5@gmail.com>
2025-12-06 18:01:58 +0200

    cleanup.sh: Update

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,4 +4,9 @@ rm -f /etc/ssh/ssh_host_*
 apt -y autoremove --purge
 apt -y clean
 apt -y autoclean
-cloud-init clean --logs --machine-id --seed
+
+if [ "$(command -v cloud-init)" ]; then
+    cloud-init clean --logs --machine-id --seed
+else
+    truncate -s 0 /etc/machine-id
+fi
